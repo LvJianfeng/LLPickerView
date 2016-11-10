af89764426d0cc8c7a61dd85af2a2b326237b1cb
Crazyf <coderjianfeng@foxmail.com>
2016-11-10 17:03:25 +0800

    Update ViewController.m

--- Demo/ViewController.m
+++ Demo/ViewController.m
@@ -22,7 +22,7 @@
 // 点击弹出(双按钮样式)
 - (IBAction)style1:(id)sender {
     LLPickerView *pickerView = [[LLPickerView alloc] initWithData:@[@"前天",@"昨天",@"今天"]];
-    pickerView.confirmBlcok = ^(NSInteger row, id object){
+    pickerView.confirmBlock = ^(NSInteger row, id object){
         NSLog(@"%ld--%@",row,object);
     };
     [self.view addSubview:pickerView];
@@ -31,10 +31,10 @@
 // 点击弹出(其他按钮样式)
 - (IBAction)style2:(id)sender {
     LLPickerView *pickerView = [[LLPickerView alloc] initWithData:@[@"前天",@"昨天",@"今天"] title:nil leftTitle:@"新建信息" rightTitle:@"确认"];
-    pickerView.confirmBlcok = ^(NSInteger row, id object){
+    pickerView.confirmBlock = ^(NSInteger row, id object){
         NSLog(@"%ld--%@",row,object);
     };
-    pickerView.cancelBlcok = ^{
+    pickerView.cancelBlock = ^{
         NSLog(@"新建");
     };
     [self.view addSubview:pickerView];
@@ -43,7 +43,7 @@
 // 点击弹出(无标题)
 - (IBAction)style3:(id)sender {
     LLPickerView *pickerView = [[LLPickerView alloc] initWithData:@[@"疾风剑豪卡",@"剑魔"] title:nil leftTitle:@"新建" rightTitle:@"确认"];
-    pickerView.confirmBlcok = ^(NSInteger row, id object){
+    pickerView.confirmBlock = ^(NSInteger row, id object){
         NSLog(@"%ld--%@",row,object);
     };
     pickerView.cancelBlcok = ^{
@@ -55,10 +55,10 @@
 // 点击弹出(有标题)
 - (IBAction)style4:(id)sender {
     LLPickerView *pickerView = [[LLPickerView alloc] initWithData:@[@"疾风剑豪哇塞卡疾风剑豪哇塞卡",@"剑魔剑魔剑魔剑魔剑魔剑魔"] title:@"选择" leftTitle:nil rightTitle:@"确认"];
-    pickerView.confirmBlcok = ^(NSInteger row, id object){
+    pickerView.confirmBlock = ^(NSInteger row, id object){
         NSLog(@"%ld--%@",row,object);
     };
-    pickerView.cancelBlcok = ^{
+    pickerView.cancelBlock = ^{
         NSLog(@"新建");
     };
     [self.view addSubview:pickerView];
@@ -69,10 +69,10 @@
     LLPickerView *pickerView = [[LLPickerView alloc] initWithData:@[@"疾风剑豪哇塞卡疾风剑豪哇塞卡",@"剑魔剑魔剑魔剑魔剑魔剑魔"] title:@"选择" leftTitle:@"新建" rightTitle:@"确认"];
     pickerView.isAllow = YES;
     
-    pickerView.confirmBlcok = ^(NSInteger row, id object){
+    pickerView.confirmBlock = ^(NSInteger row, id object){
         NSLog(@"%ld--%@",row,object);
     };
-    pickerView.cancelBlcok = ^{
+    pickerView.cancelBlock = ^{
         NSLog(@"新建");
     };
     [self.view addSubview:pickerView];
